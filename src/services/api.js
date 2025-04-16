99b0be096b2e4962e18c73acc27671b2623c9aea
Vitor <vitorpereira@profkenny.com.br>
2025-04-16 10:26:47 -0300

    Definindo variavel de ambiente para api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 })
+
 
 // Interceptor para adicionar o token dinamicamente
 api.interceptors.request.use(config => {
